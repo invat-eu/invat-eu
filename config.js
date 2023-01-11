fafb4bacd9ac913862ca56cfe7b57b4ae50a8b50
App Generator <51070104+app-generator@users.noreply.github.com>
2023-01-11 21:19:08 +0200

    HOMEpage Site

---
+++ config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  siteTitle: 'Invat.eu - Resurse Si Servicii pentru sistemul Educational', // <title>
+  manifestName: 'Aerial',
+  manifestShortName: 'Portfolio', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/gatsby-starter-aerial/`, // This path is subpath of your hosting https://domain/portfolio
+  // social
+
+  authorName: 'Invat.eu',
+  authorHeading: 'Resurse si servicii pentru sistemul educational din Romania.',
+  authorSocialLinks: [
+    {
+      icon: 'fa-facebook',
+      name: 'Facebook',
+      url: 'https://www.facebook.com/invat.eu',
+    },
+    {
+      icon: 'fa-twitter',
+      name: 'Twitter',
+      url: 'https://twitter.com/eu_invat',
+    },
+    {
+      icon: 'fa-envelope-o',
+      name: 'Email',
+      url: 'mailto:contact@invat.eu',
+    },
+  ],
+};
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer id="footer">
+      <span class="copyright">
+        &copy; Invat.eu - Sustinut de <a target="_blank" rel="noreferrer noopener" href="https://rosoftware.ro/">RoSoftware</a>.
+      </span>
+    </footer>
+  );
+}
